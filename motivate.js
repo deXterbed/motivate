d01070c4342e009d8a929b8e432fb06326bca9c3
Manoj Mishra <b0rn2c0d3@gmail.com>
2015-01-14 02:29:01 +0530

    Seed with timestamp instead of date

--- motivate.js
+++ motivate.js
@@ -3,7 +3,7 @@ var moment = require('moment');
 var quotes = require('./quotes.json');
 
 module.exports = function() {
-  var seed = parseInt(moment().format('YYYYMMDD'));
+  var seed = parseInt(moment().format('hmmss'));
   var engine = R.engines.mt19937().seed(seed);
   var index = R.integer(0, quotes.length - 1)(engine);
   return quotes[index];
